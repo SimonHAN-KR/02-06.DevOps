4eccac2fddac4f8352496482a1ec34d6e18fba32
SimonHAN-KR <98280861+SimonHAN-KR@users.noreply.github.com>
2024-05-09 16:42:47 +0900

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -9,13 +9,26 @@ echo ssh user password: $SSH_USERPASS
 usermod -aG wheel terra
 }
 
+__azurecli() {
+# Create a user to SSH into as.
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo; \
+dnf install -y azure-cli
+}
+
 __codeserver() {
 # Create a user to SSH into as.
 wget https://github.com/cdr/code-server/releases/download/v4.9.0/code-server-4.9.0-amd64.rpm
-sudo rpm -ivh ./code-server-4.9.0-amd64.rpm
+rpm -ivh ./code-server-4.9.0-amd64.rpm
 }
 
 
 # Call all functions
 __create_user
+__azurecli
 __codeserver
